1971ec279dd3e564ca91d5fe0fa71e0832a949fa
Yarit <yarit.villalobos@mail.escuelaing.edu.co>
2021-09-22 15:22:50 -0500

    agregando vista

--- src/main/java/edu/edi/practicaDocker/App.java
+++ src/main/java/edu/edi/practicaDocker/App.java
@@ -10,6 +10,7 @@ public class App {
     public static void main(String [] args){
         port(getPort());
         get("hello", (req,res) -> "Hello Docker!");
+        get("palabras", (req, res) -> devolderHtml(req,res));
   }
 
   private static int getPort() {
@@ -17,5 +18,29 @@ public class App {
           return Integer.parseInt(System.getenv("PORT"));
       }
       return 4567;
+  }
+  
+  public static String devolderHtml(Request req, Response res) {
+	  return "<!DOCTYPE html>\r\n"
+	  		+ "<html>\r\n"
+	  		+ "    <head>\r\n"
+	  		+ "        <meta charset=\\\"UTF-8\\\">\r\n"
+	  		+ "        <title>title og the document</title>\r\n"
+	  		+ "        <script src=\"https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js\"></script>\r\n"
+	  		+ "        <script src=\"/index.js\"></script>\r\n"
+	  		+ "        <link rel=\"stylesheet\" href=\"/index.css\"/>\r\n"
+	  		+ "        <style>\r\n"
+	  		+ "            body { background-color: RED; }\r\n"
+	  		+ "        </style>\r\n"
+	  		+ "    </head>\r\n"
+	  		+ "    <body>\r\n"
+	  		+ "        <center>\r\n"
+	  		+ " \r\n"
+	  		+ "            <input id=\"entrada\" type=\"text\" name=\"Compania\" placeholder=\"Ingrese un dato\">\r\n"
+	  		+ "            <br><button type=\"button\" class=\"button\" onclick=\"index.change($('#entrada').val())\">Click me</button>\r\n"
+	  		+ " \r\n"
+	  		+ "        </center>\r\n"
+	  		+ "    </body>\r\n"
+	  		+ "</html>";
   }
 }
