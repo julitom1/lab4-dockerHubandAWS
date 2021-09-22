9ad58e0c075e7bbb3f4c50cd68cbaa9cfa515e1c
root <yarit.villalobos@mail.escuelaing.edu.co>
2021-09-22 13:29:04 -0500

    lo que llevo

---
+++ src/main/java/edu/edi/practicaDocker/App.java
@@ -0,0 +1,21 @@
+package edu.edi.practicaDocker;
+import spark.Request;
+import spark.Filter;
+import static spark.Spark.*;
+import spark.Response;
+
+
+public class App {
+
+    public static void main(String [] args){
+        port(getPort());
+        get("hello", (req,res) -> "Hello Docker!");
+  }
+
+  private static int getPort() {
+      if (System.getenv("PORT") != null) {
+          return Integer.parseInt(System.getenv("PORT"));
+      }
+      return 4567;
+  }
+}
